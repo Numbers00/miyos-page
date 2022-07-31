f1e07a5b95dfc5be2cbc16ea373321a88737040d
Numbers00 <kmaquino@up.edu.ph>
2022-07-31 19:14:55 +0800

    added form submit eligibility feature

--- script.js
+++ script.js
@@ -35,19 +35,40 @@ const ocSlider = tns({
 
 // code adapted from and thanks to
 // https://codepen.io/bstonedev/pen/MWWZgKz
-let elementsArray = document.querySelectorAll(".has-fade");
-window.addEventListener('scroll', fadeIn); 
+const fadeElems = document.querySelectorAll(".has-fade");
+window.addEventListener('scroll', fadeIn);
 function fadeIn () {
-    for (var i = 0; i < elementsArray.length; i++) {
-        let elem = elementsArray[i];
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
-        console.log(distInView);
-        if (distInView < 0) {
-            elem.classList.add("in-view");
-        } else {
-            elem.classList.remove("in-view");
-        }
+  for (var i = 0; i < fadeElems.length; i++) {
+    let elem = fadeElems[i];
+    let distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+    console.log(distInView);
+    if (distInView < 0) {
+      elem.classList.add("in-view");
+    } else {
+      elem.classList.remove("in-view");
     }
+  }
 }
 fadeIn();
 // end adapted code
+
+document.querySelector('#nameInput').addEventListener('keyup', checkSubmitEligibility);
+document.querySelector('#emailInput').addEventListener('keyup', checkSubmitEligibility);
+document.querySelector('#typeInput').addEventListener('keyup', checkSubmitEligibility);
+document.querySelector('#sizeInput').addEventListener('keyup', checkSubmitEligibility);
+document.querySelector('#colorInput').addEventListener('keyup', checkSubmitEligibility);
+document.querySelector('#background').addEventListener('keyup', checkSubmitEligibility);
+function checkSubmitEligibility () {
+  const name = document.getElementById('nameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const type = document.getElementById('typeInput').value;
+  const size = document.getElementById('sizeInput').value;
+  const color = document.getElementById('colorInput').value;
+  const background = document.getElementById('background').value;
+  const submitBtn = document.getElementById('submit-btn');
+  if (name && email && type && size && color && background) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
