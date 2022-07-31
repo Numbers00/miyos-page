2bb9925a51527cb95dc4eb3ef1dc3a6346f7565a
Numbers00 <kmaquino@up.edu.ph>
2022-07-31 17:45:23 +0800

    finished up to ocs-section

---
+++ script.js
@@ -0,0 +1,34 @@
+const drawingSlider = tns({
+  container: '.drawings-slider',
+  items: 4,
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  controls: false,
+  nav: false,
+  speed: 400
+});
+
+const videoSlider = tns({
+  container: '.videos-slider',
+  center: true,
+  items: 4,
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  controls: false,
+  nav: false,
+  speed: 400
+});
+
+const ocSlider = tns({
+  container: '.ocs-slider',
+  center: true,
+  items: 3,
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  controls: false,
+  nav: false,
+  speed: 400
+});
